e5d94c4a9baf171c49c9da3083f2d3558fa9e39c
Matheus Hans Duffeck de Oliveira <69155648+Hans-Oliveira@users.noreply.github.com>
2023-02-16 00:14:54 -0300

    Atualizando produtos
    Ajustando mais alguns produtos no catálogo.

--- js/scripts.js
+++ js/scripts.js
@@ -10,11 +10,22 @@ $( document ).ready(function() {
 /*  */
 
 $(".whats").click(function() {
-    console.log();
+    
     let produto = $(this).parents(".card").children(".card-body").children().text();
 
     let textWhatsapp = `Junio, Gostaria de saber um pouco mais sobre o porduto ${produto}, quantidades e valores`
     textWhatsapp = textWhatsapp.replace(/\s/g, "%20");
 
     window.location.href = `https://api.whatsapp.com/send?phone=5517997323133&text=${textWhatsapp}`;
+});
+
+$(".whats2").click(function() {
+
+    let produto = $(this).parent().find('h2').text();
+
+    let textWhatsapp = `Junio, Gostaria de saber um pouco mais sobre o porduto ${produto}, quantidades e valores`
+    textWhatsapp = textWhatsapp.replace(/\s/g, "%20");
+
+    window.location.href = `https://api.whatsapp.com/send?phone=5517997323133&text=${textWhatsapp}`;
+    
 });
