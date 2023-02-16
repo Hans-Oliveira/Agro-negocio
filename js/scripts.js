f518f36c9dcac7bddc1d99a4ee846a0a25352262
Matheus Hans Duffeck de Oliveira <69155648+Hans-Oliveira@users.noreply.github.com>
2023-02-15 23:58:56 -0300

    Atualização do javascript
    Colocando para iniciar conversa no whatsapp com o nome do produto

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,20 @@
 $( document ).ready(function() {
     
- });
+});
 
 /* function abrirModalAction() {
     $('#exampleModalCenter').modal('show'); 
 } */
+
+
+/*  */
+
+$(".whats").click(function() {
+    console.log();
+    let produto = $(this).parents(".card").children(".card-body").children().text();
+
+    let textWhatsapp = `Junio, Gostaria de saber um pouco mais sobre o porduto ${produto}, quantidades e valores`
+    textWhatsapp = textWhatsapp.replace(/\s/g, "%20");
+
+    window.location.href = `https://api.whatsapp.com/send?phone=5517997323133&text=${textWhatsapp}`;
+});
